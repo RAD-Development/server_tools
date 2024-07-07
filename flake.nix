fde25f9bb4b704cd1f4358c3ff4ff910dd0a3e5b
Richie Cahill <Richie@tmmworkshop.com>
2024-07-07 10:50:04 -0400

    formatted and cleaned up flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A tool to find and remove reddit watermarks.";
+  description = "tools for our server";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -13,54 +13,52 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      lib = pkgs.lib;
-      poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;};
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
 
-      p2nix_defaults = {
-        projectDir = ./.;
-        python = pkgs.python311;
-        overrides = poetry2nix.overrides.withDefaults (
-          _: prev:
-            lib.genAttrs [
-              "ruff"
-            ]
-            (package: prev.${package}.override {preferWheel = true;})
-        );
-      };
-    in {
-      packages = {
-        default = poetry2nix.mkPoetryApplication p2nix_defaults;
-      };
+        p2nix_defaults = {
+          projectDir = ./.;
+          python = pkgs.python311;
+          overrides = poetry2nix.overrides.withDefaults (
+            _: prev: lib.genAttrs [ "ruff" ] (package: prev.${package}.override { preferWheel = true; })
+          );
+        };
+      in
+      {
+        packages.default = poetry2nix.mkPoetryApplication p2nix_defaults;
 
-      devShells = {
-        default =
-          (poetry2nix.mkPoetryEnv p2nix_defaults
+        devShells.default =
+          (
+            poetry2nix.mkPoetryEnv p2nix_defaults
             // {
               editablePackageSources = {
                 server_tools = ./server_tools;
               };
-            })
-          .env
-          .overrideAttrs (old: {
-            buildInputs = with pkgs; [
-              poetry
-              just
-              python311Packages.pudb
-            ];
-          });
-      };
+            }
+          ).env.overrideAttrs
+            (old: {
+              buildInputs = with pkgs; [
+                poetry
+                just
+                python311Packages.pudb
+              ];
+            });
 
-      apps.default = {
+        apps.default = {
           type = "app";
           program = "${self.packages.server_tools}/bin/validate_jeeves";
         };
-    });
+      }
+    );
 }
